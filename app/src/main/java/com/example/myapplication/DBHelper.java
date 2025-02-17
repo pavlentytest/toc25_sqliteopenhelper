50004557b233ceb5b95517de5401fe93ec055dce
Pavel Stepanov <impetus@yandex.ru>
2025-02-17 19:18:36 +0300

    getall

---
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "users2025.db";
+    public static final String TABLE_NAME = "users";
+    public static final int SCHEMA = 1; // версия БД
+
+    // Название столбцов
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_YEAR = "year";
+
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+TABLE_NAME+ "( "
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_NAME + " TEXT, "
+                + COLUMN_YEAR + " INTEGER); "
+                );
+        db.execSQL("INSERT INTO "+TABLE_NAME + "("+COLUMN_NAME+","+COLUMN_YEAR+") VALUES ('Ivanov Ivan','2010')");
+    }
+
+    public ArrayList<User> getAll() {
+        ArrayList<User> userArrayList = new ArrayList<>();
+        String sql = "SELECT * FROM "+TABLE_NAME;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(sql,null);
+        if(cursor.moveToFirst()) {
+            do {
+                User user = new User(cursor.getString(1),Integer.parseInt(cursor.getString(2)));
+                userArrayList.add(user);
+            } while  (cursor.moveToNext());
+        }
+        return userArrayList;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
